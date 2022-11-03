ae913907887393616924c0ee4996ddeebb2d8342
dongho <dongho108@naver.com>
2022-11-03 16:05:01 +0900

    test: OrderItems 테스트 추가

--- src/test/java/com/musinsa/domain/OrderItemTest.java
+++ src/test/java/com/musinsa/domain/OrderItemTest.java
@@ -15,4 +15,18 @@ class OrderItemTest {
         final OrderItem orderItem = getOrderItemRequest(getCartProductRequest(getProduct(1L, 1000), 10));
         assertThat(orderItem.getAmount()).isEqualTo(BigDecimal.valueOf(10000));
     }
+
+    @Test
+    void 주문상품이_주어진상품과_같으면_true_를_반환한다() {
+        final Product product = getProduct(1L, 1000);
+        final OrderItem orderItem = getOrderItemRequest(getCartProductRequest(product, 10));
+        assertThat(orderItem.isThisProduct(product)).isTrue();
+    }
+
+    @Test
+    void 주문상품이_주어진상품과_다르면_false_를_반환한다() {
+        final Product product = getProduct(1L, 1000);
+        final OrderItem orderItem = getOrderItemRequest(getCartProductRequest(getProduct(2L, 1000), 10));
+        assertThat(orderItem.isThisProduct(product)).isFalse();
+    }
 }
